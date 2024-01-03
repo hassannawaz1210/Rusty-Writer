e97ba14d7d46ae5059a3752fb603458fd832120a
Abdul Wahab <118908180+awahab111@users.noreply.github.com>
2024-01-03 15:41:27 +0500

    Fixed the dual input
    Created a new function which read_keys which returns the key even of kind pressed rather than both press and release

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,52 @@
-use crossterm::{cursor, event::{self, Event, KeyCode}, execute};
+use crossterm::{event::KeyEventKind, terminal::{enable_raw_mode, disable_raw_mode}};
+pub use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue, style,
+    terminal::{self, ClearType},
+    Command,
+};
+
 use std::io::{stdout, Write};
 
-fn main() {
+
+pub fn read_key() -> crossterm::Result<KeyEvent> {
+    loop {
+        if let Ok(Event::Key(key_event)) = event::read() {
+            if key_event.kind == KeyEventKind::Press {
+                return Ok(key_event);
+            }
+        }
+    }
+}
+
+fn main()  -> crossterm::Result<()>{
+
+    //TODO: what is the use of ? 
 
     let mut stdout = stdout();
 
-    println!("Started.");
-    //clear the screen
-    print!("{esc}c", esc = 27 as char);
+    execute!(stdout, terminal::EnterAlternateScreen)?;
+
+    terminal::enable_raw_mode()?; // read somewhere does not echo the user input in raw mode
+
+    execute!(
+        stdout,
+        style::ResetColor,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::MoveTo(0, 0),
+        cursor::SetCursorStyle::BlinkingUnderScore
+    )?; // Macro within crossterm for setting up the terminal
+
+
 
     loop {
-        if event::poll(std::time::Duration::from_millis(1000000000000000000)).unwrap() {
-            if let Event::Key(key_event) = event::read().unwrap() {
+        // if event::poll(std::time::Duration::from_secs(10)).unwrap() { // For some reason this does nothing to the code 
+            if let Ok(key_event) = read_key() {
                 match key_event.code {
                     KeyCode::Char(c) => {
-                        stdout.flush().unwrap();
                         print!("{}", c);
+                        stdout.flush().unwrap();
                     },
                     KeyCode::Backspace => {
                         //remove char
@@ -30,6 +61,9 @@ fn main() {
                         }
                         stdout.flush().unwrap();
                     },
+                    KeyCode::Esc => {
+                        break;
+                    },
                     KeyCode::Enter => println!(),
                     KeyCode::Up => execute!(stdout, cursor::MoveUp(1)).unwrap(),
                     KeyCode::Down => execute!(stdout, cursor::MoveDown(1)).unwrap(),
@@ -38,6 +72,9 @@ fn main() {
                     _ => (),
                 }
             }
-        }
+        // }
     }
+    execute!(stdout, terminal::LeaveAlternateScreen)?; // Leave alternate screen
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
